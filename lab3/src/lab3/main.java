55175116b4cd0aa5621cc2ccd7708ae9b4fd328c
tharukihettihandi <nuwatharu@gmail.com>
2020-03-08 03:20:52 -0700

    first commit

---
+++ lab3/src/lab3/main.java
@@ -0,0 +1,11 @@
+package lab3;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is My First Project at SLIIT!");
+
+	}
+
+}
